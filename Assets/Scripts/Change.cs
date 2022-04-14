c29dc25723665b612e27e9048bef1ca5e5d10faf
Madeha Tahboub <madehatahboub@gmail.com>
2022-04-14 13:23:48 +0300

    delete some folders + fix select level

---
+++ Assets/Scripts/Change.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Change : MonoBehaviour
+{
+    public static float moveSpeed = 6;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.back * Time.deltaTime * moveSpeed, Space.World);
+    }
+}
